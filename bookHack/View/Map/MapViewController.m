a24a1268aa55a8c7b437f3e5c851b6e272ac4ee2
Su PingChen <supc@me.com>
2017-03-11 18:17:36 +0800

    Add sample data for debug

--- bookHack/View/Map/MapViewController.m
+++ bookHack/View/Map/MapViewController.m
@@ -32,17 +32,29 @@
         _userSearchItem = [PCSearchItem new];
         _userSearchItem.location = [[CLLocation alloc] initWithLatitude:-33.880837 longitude:151.205606];
         _sceneMapItems = @[
-                           [[PCMapBaseItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.880837 longitude:151.205606]
-                                                              name:@"Scene A"],
-                           [[PCMapBaseItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.881837 longitude:151.205606]
-                                                              name:@"Scene B"],
-                           [[PCMapBaseItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.880837 longitude:151.202606]
-                                                              name:@"Scene C"]
+                           [[PCMapSceneItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.880837 longitude:151.205606]
+                                                               name:@"Scene A"],
+                           [[PCMapSceneItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.881837 longitude:151.205606]
+                                                               name:@"Scene B"],
+                           [[PCMapSceneItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.880837 longitude:151.202606]
+                                                               name:@"Scene C"]
+                           ];
+        _hotelMapItems = @[
+                           [[PCMapHotelItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.870837 longitude:151.205606]
+                                                               name:@"Scene A"],
+                           [[PCMapHotelItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.871837 longitude:151.201606]
+                                                               name:@"Scene B"],
+                           [[PCMapHotelItem alloc] initWithLocation:[[CLLocation alloc] initWithLatitude:-33.880737 longitude:151.222606]
+                                                               name:@"Scene C"]
                            ];
     }
     
     mapView.region = MKCoordinateRegionMakeWithDistance(_userSearchItem.location.coordinate, 2000, 2000);
     for (PCMapBaseItem *mapItem in _sceneMapItems) {
+        [mapView addAnnotation:mapItem];
+    }
+    for (PCMapBaseItem *mapItem in _hotelMapItems) {
+        [mapView addAnnotation:mapItem];
     }
 }
 
@@ -52,9 +64,16 @@
 }
 
 - (IBAction)someAction:(id)sender {
-    [filterViewController willMoveToParentViewController:self];
-    [[filterViewController view] removeFromSuperview];
-    [filterViewController removeFromParentViewController];
+    if (filterViewController != nil) {
+        [filterViewController willMoveToParentViewController:self];
+        [[filterViewController view] removeFromSuperview];
+        [filterViewController removeFromParentViewController];
+        [UIView animateWithDuration:0.3 animations:^{
+            [self.view layoutIfNeeded];
+        }];
+        filterViewController = nil;
+        return;
+    }
     
     UIViewController *vc = [UIViewController new];
     [self addChildViewController:vc];
@@ -95,6 +114,17 @@
 
 - (void)mapViewDidFinishRenderingMap:(MKMapView *)mapView fullyRendered:(BOOL)fullyRendered {
     
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+    if ([annotation isKindOfClass: [PCMapHotelItem class]]) {
+        annotationView.pinTintColor = [UIColor redColor];
+    }
+    if ([annotation isKindOfClass: [PCMapSceneItem class]]) {
+        annotationView.pinTintColor = [UIColor blueColor];
+    }
+    return annotationView;
 }
 
 - (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray<MKAnnotationView *> *)views {
