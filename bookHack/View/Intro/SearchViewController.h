f036148e60d8348c5d22aba0bdbe6efacdaba750
Jack KY Chen <jackkychen@kkstream.com.tw>
2017-03-11 18:18:33 +0800

    add searchView

---
+++ bookHack/View/Intro/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  bookHack
+//
+//  Created by Jack KY Chen on 2017/3/11.
+//  Copyright © 2017年 Jack KY Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SearchViewController : ViewController
+
+@end
---
+++ bookHack/View/Intro/SearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  SearchViewController.m
+//  bookHack
+//
+//  Created by Jack KY Chen on 2017/3/11.
+//  Copyright © 2017年 Jack KY Chen. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
